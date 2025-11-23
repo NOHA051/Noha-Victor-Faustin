39d61ab29ba83d3ce3b57ae9067ca00735c21c1c
VictorKaspereit <victor.kaspereit@efrei.net>
2025-11-23 13:20:12 +0100

    Update matrix.h pour etape 2

--- src/matrix.h
+++ src/matrix.h
@@ -2,20 +2,24 @@
 #define MATRIX_H
 
 #include <stdlib.h>
+#include <math.h>
 
-// Création d'une matrice n x n remplie de 0
+typedef struct {
+    int n;          // taille de la matrice (n x n)
+    double **data;  // tableau 2D
+} t_matrix;
+
 double** create_zero_matrix(int n);
 
-// Recopie d'une matrice dans une autre
 void copy_matrix(int n, double** source, double** dest);
 
-// Multiplication de deux matrices n x n
 double** multiply_matrices(int n, double** A, double** B);
 
-// Calcul de la différence entre deux matrices
 double diff_matrices(int n, double** A, double** B);
 
-// Création d'une matrice de transition à partir d'une liste d'adjacence
 double** adjacency_to_transition_matrix(int n, int** adjacency_list, int* degrees);
+
+#include "tarjan.h"
+t_matrix subMatrix(t_matrix matrix, t_partition part, int compo_index);
 
 #endif
